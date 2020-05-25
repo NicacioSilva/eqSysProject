4fd6dc332252429b7922c53f3ddd4f3763b24434
NicacioSilva <nas@cesar.school>
2020-05-25 18:32:05 -0300

    add view and home tamplate

--- eqSysProject/settings.py
+++ eqSysProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'eqSysProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'eqSysApp/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
