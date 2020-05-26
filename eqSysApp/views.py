27916ff4b71b046f90ac2fe22a060403bb9de912
NicacioSilva <nas@cesar.school>
2020-05-26 11:43:49 -0300

    setting, getting and show the constants

--- eqSysApp/views.py
+++ eqSysApp/views.py
@@ -4,10 +4,19 @@ from django.shortcuts import render
 def home(request):
     context = {}
     if request.method == 'POST':
-        number = float(request.POST.get("number"))
-        result = number**2
+        a1 = float(request.POST.get("a1"))
+        b1 = float(request.POST.get("b1"))
+        c1 = float(request.POST.get("c1"))
+        a2 = float(request.POST.get("a2"))
+        b2 = float(request.POST.get("b2"))
+        c2 = float(request.POST.get("c2"))
+
         context = {
-            'number': number,
-            'result': result,
+            'a1':a1,
+            'b1':b1,
+            'c1':c1,
+            'a2':a2,
+            'b2':b2,
+            'c2':c2,
         }
     return render(request, 'eqSysApp/home.html', context)
