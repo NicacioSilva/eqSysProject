22e372f45f99e3c129395d3b306c4724e284b8f9
NicacioSilva <nas@cesar.school>
2020-05-29 10:41:28 -0300

    add an about page.

--- eqSysApp/views.py
+++ eqSysApp/views.py
@@ -13,3 +13,7 @@ def home(request):
         from eqSysApp.sympy.sympy import sys_2x2
         context = sys_2x2(a1, b1, c1, a2, b2, c2)
     return render(request, 'eqSysApp/home.html', context)
+
+
+def about(request):
+    return render(request, 'eqSysApp/about.html')
