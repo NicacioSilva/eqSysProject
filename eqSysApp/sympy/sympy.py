6eff94a07b7817f4b977bcaf63e18e84c8680626
NicacioSilva <nas@cesar.school>
2020-05-29 21:28:49 -0300

    if number is 1.0, for ex., it will show just 1 in the solution system

--- eqSysApp/sympy/sympy.py
+++ eqSysApp/sympy/sympy.py
@@ -1,20 +1,32 @@
 from sympy import *
+import math
+
 
 def sys_2x2(a1, b1, c1, a2, b2, c2):
     coefficients = {
-        'a1': a1,
-        'b1': b1,
-        'c1': c1,
-        'a2': a2,
-        'b2': b2,
-        'c2': c2,
+        'a1': dec_to_int(a1),
+        'b1': dec_to_int(b1),
+        'c1': dec_to_int(c1),
+        'a2': dec_to_int(a2),
+        'b2': dec_to_int(b2),
+        'c2': dec_to_int(c2),
     }
     system = Matrix(((a1, b1, c1), (a2, b2, c2)))
     dictionary = solve_linear_system(system, 'x', 'y')
-    dictionary.update(coefficients)
+
     x, y = symbols('x y')
-    expr_1 = latex(a1 * x + b1 * y)
-    expr_2 = latex(a2 * x + b2 * y)
+    expr_1 = latex(dec_to_int(a1) * x + dec_to_int(b1) * y)
+    expr_2 = latex(dec_to_int(a2) * x + dec_to_int(b2) * y)
     expression = {'expr_1': expr_1, 'expr_2': expr_2}
+
+    dictionary.update(coefficients)
     dictionary.update(expression)
     return dictionary
+
+
+def dec_to_int(number):
+    dec, num = math.modf(number)
+    if dec == 0:
+        return int(number)
+    else:
+        return number
