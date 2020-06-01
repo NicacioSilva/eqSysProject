368a7b635c6103ea32aac93e152f1a31b54ce231
NicacioSilva <nas@cesar.school>
2020-06-01 18:15:59 -0300

    home view takes args from POST method in a better way.

--- eqSysApp/sympy/sympy.py
+++ eqSysApp/sympy/sympy.py
@@ -1,8 +1,19 @@
 from sympy import *
 import math
 
+def sys_manager(**kwargs):
+    ''' It receives the args and decides what system function
+        will process everything returns a dictionary with the
+        appropriate content. '''
+    pass
 
-def sys_2x2(a1, b1, c1, a2, b2, c2):
+def sys_2x2(dic):
+    a1 = dic.get('a1')
+    b1 = dic.get('b1')
+    c1 = dic.get('c1')
+    a2 = dic.get('a2')
+    b2 = dic.get('b2')
+    c2 = dic.get('c2')
     a1, b1, c1, a2, b2, c2 = dec_to_int(a1), dec_to_int(b1), dec_to_int(c1), dec_to_int(a2), dec_to_int(b2), dec_to_int(c2)
     coefficients = {
         'a1': a1,
@@ -12,6 +23,9 @@ def sys_2x2(a1, b1, c1, a2, b2, c2):
         'b2': b2,
         'c2': c2,
     }
+    # test if linear system have a solution and validate:
+    # if yes: call the steps function and pass the params
+    # if not: return an advise message.
     system = Matrix(((a1, b1, c1), (a2, b2, c2)))
     dictionary = solve_linear_system(system, 'x', 'y')
 
@@ -26,7 +40,7 @@ def sys_2x2(a1, b1, c1, a2, b2, c2):
 
 
 def dec_to_int(number):
-    dec, num = math.modf(number)
+    dec, num = math.modf(float (number))
     if dec == 0:
         return int(number)
     else:
--- eqSysApp/views.py
+++ eqSysApp/views.py
@@ -3,15 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     context = {}
+
     if request.method == 'POST':
-        a1 = float(request.POST.get("a1"))
-        b1 = float(request.POST.get("b1"))
-        c1 = float(request.POST.get("c1"))
-        a2 = float(request.POST.get("a2"))
-        b2 = float(request.POST.get("b2"))
-        c2 = float(request.POST.get("c2"))
         from eqSysApp.sympy.sympy import sys_2x2
-        context = sys_2x2(a1, b1, c1, a2, b2, c2)
+        context = sys_2x2(request.POST.dict())
+
     return render(request, 'eqSysApp/home.html', context)
 
 
