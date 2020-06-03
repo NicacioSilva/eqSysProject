5eecf964ce11a3841090088be13e9fa5b45e089e
NicacioSilva <nas@cesar.school>
2020-06-03 14:36:19 -0300

    step 2 done

--- eqSysApp/sympy/steps_2x2_substitution.py
+++ eqSysApp/sympy/steps_2x2_substitution.py
@@ -30,7 +30,7 @@ def system_2x2_solution_steps(a1, b1, c1, a2, b2, c2):
     step_body = []
     body_1 = ''
     if a1 != 0:
-        body_1 = r'\begin{align} \text{Isolate x of }' + \
+        body_1 = r'\begin{align} \text{Isolate x for equation (i): }' + \
                  f'{latex(a1 * x + b1 * y)}' + \
                  r'& =' + \
                  f'{c1} ' + \
@@ -39,6 +39,22 @@ def system_2x2_solution_steps(a1, b1, c1, a2, b2, c2):
                  f'{latex(factor((c1 - b1 * y) / (a1)))}' + \
                  r'\end{align}'
         step_body.append(body_1)
+    steps.append({'step_name': step_name, 'step_body': step_body})
+
+    step_name = 'Step 2'
+    step_body = []
+    body_1 = r'\begin{align} \text{Substitute x: }' + \
+             r' x & =' + \
+             f'{latex(factor((c1 - b1 * y) / (a1)))}' + \
+             r'\text{ in the equation (ii): }' + \
+             r'\\' + \
+             f'{latex(a2 * UnevaluatedExpr((c1 - b1 * y) / (a1)))}' + \
+             f'{latex( + b2 * y)}' + \
+             r'& = ' + \
+             f'{c2}' + \
+             r'\end{align}'
+
+    step_body.append(body_1)
     steps.append({'step_name': step_name, 'step_body': step_body})
 
     return steps
